7583dd291e8496ca13c60828165af8de80a847ba
emilybaddeleywhite-cmd <emilybaddeleywhite@gmail.com>
2026-03-29 09:38:09 +0100

    Update generate-script.js

--- pages/api/generate-script.js
+++ pages/api/generate-script.js
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+export const config = {
+  maxDuration: 60,
+}
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
